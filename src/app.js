9d5622848d8344190bd6b9c9586ce26a078c36c6
unknown <lalithkanaka2@gmail.com>
2025-10-11 04:03:08 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+// Express js is used to create a new HTTP server
+const express = require('express')
+
+const app = express()
+
+app.use((req, res) => {
+  res.send('Hello')
+})
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000')
+})
