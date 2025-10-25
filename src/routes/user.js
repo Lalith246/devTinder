4e3429010aac798e1873067d11630c46a99f79c9
unknown <lalithkanaka2@gmail.com>
2025-10-25 12:24:38 +0530

    User Feed API

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,9 +2,14 @@ const express = require('express')
 const mongoose = require('mongoose')
 
 const { userAuth } = require('../middlewares/auth')
+
+const { userModel } = require('../models/user')
 const { connectionModel } = require('../models/connectionRequest')
 
 const userRouter = express.Router()
+
+const USER_COMMON_DATA =
+  'firstName lastName DOB gender about games otherGames photoUrl'
 
 userRouter.get('/user/connections', userAuth, async (req, res) => {
   // Get the logged in userId
@@ -18,8 +23,8 @@ userRouter.get('/user/connections', userAuth, async (req, res) => {
         { receiverId: loggedInUserId, connectionStatus: 'accepted' },
       ],
     })
-    .populate('senderId', 'firstName lastName age gender about')
-    .populate('receiverId', 'firstName lastName age gender about')
+    .populate('senderId', USER_COMMON_DATA)
+    .populate('receiverId', USER_COMMON_DATA)
 
   userConnections = userConnections.map((item) =>
     item.senderId._id.toString() === loggedInUserId
@@ -44,13 +49,7 @@ userRouter.get('/user/requests/received', userAuth, async (req, res) => {
       receiverId: loggedInUserId,
       connectionStatus: 'interested',
     })
-    .populate('senderId', [
-      'firstName',
-      'LastName',
-      'photoUrl',
-      'about',
-      'gender',
-    ])
+    .populate('senderId', USER_COMMON_DATA)
 
   pendingRequests = pendingRequests.map((item) => item.senderId)
 
@@ -59,6 +58,54 @@ userRouter.get('/user/requests/received', userAuth, async (req, res) => {
     errorString: '',
     requests: pendingRequests,
   })
+})
+
+userRouter.get('/user/feed', userAuth, async (req, res) => {
+  try {
+    const loggedInUserId = req?.userIdObject?._id
+    if (!loggedInUserId) throw new Error('Invalid request')
+
+    // Pagination
+    const page = parseInt(req.query.page)
+    const limit = parseInt(req.query.limit)
+
+    // Get the connections where the logged in user is involved.
+    const userConnections = await connectionModel
+      .find({
+        $or: [{ senderId: loggedInUserId }, { receiverId: loggedInUserId }],
+      })
+      .select('senderId receiverId')
+
+    // Need to hide all the users that are involved in a connection with the loggedin user
+    const usersToHide = new Set()
+    userConnections.forEach((element) => {
+      usersToHide.add(element.senderId)
+      usersToHide.add(element.receiverId)
+    })
+
+    // Now get all the users by filtering out the unwanted users
+    const finalFeedList = await userModel
+      .find({
+        $and: [
+          { _id: { $ne: loggedInUserId } },
+          { _id: { $nin: Array.from(usersToHide) } },
+        ],
+      })
+      .select(USER_COMMON_DATA)
+      .skip((page - 1) * limit)
+      .limit(limit)
+
+    res.json({
+      errorCode: 0,
+      errorString: '',
+      availableUsers: finalFeedList,
+    })
+  } catch (error) {
+    res.status(400).send({
+      errorCode: 1,
+      errorString: error.message,
+    })
+  }
 })
 
 module.exports = {
